cbfc1741fd63923c3959d0c3adf6ff2ae546ff4b
Kadaie <kadaie@gmail.com>
2024-12-08 16:12:16 +0630

    Second Commit

--- modules/rds/output.tf
+++ modules/rds/output.tf
@@ -8,13 +8,6 @@ output "rds_endpoint" {
   description = "RDS instance endpoint"
   value       = aws_db_instance.rds_instance.endpoint
 }
-
-# Output the RDS Security Group ID
-output "rds_security_group_id" {
-  description = "RDS security group ID"
-  value       = aws_security_group.rds_sg.id
-}
-
 # Output the Subnet Group Name
 output "rds_subnet_group_name" {
   description = "RDS subnet group name"
